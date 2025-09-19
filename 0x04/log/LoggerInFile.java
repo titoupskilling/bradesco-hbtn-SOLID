3e44295aef41e3bd4c857ad15353908e1ea1efd9
Renato <ren.tito@yahoo.com.br>
2025-09-19 01:22:55 +0000

    project estrutura de dados

---
+++ 0x04/log/LoggerInFile.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInFile {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger("MyLog");
+
+        FileHandler fileHandler;
+
+        fileHandler = new FileHandler("logs.txt");
+
+        Logger.addHandler(fileHandler);
+
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+
+        fileHandler.setFormatter(simpleFormatter);
+
+        Logger.info("teste de Log");
+
+        Logger.info("Olá, eu sou o teste da classe principal");
+
+    }
+
+}
