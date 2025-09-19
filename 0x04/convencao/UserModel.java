a48d487622f9d78758f3daf49db7873fe24868c1
Renato <ren.tito@yahoo.com.br>
2025-09-19 01:19:00 +0000

    project estrutura de dados

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,17 @@
+/**
+ * classe users, representando nossa entidade
+*/ 
+class UsersModels {
+
+    int idUser;
+    
+    String descricao;
+    
+    final static int IDADE_MINIMA = 18;
+    
+    
+    // função que coleta todos os users
+    
+    public getUsers() {return [];}
+    
+    }
